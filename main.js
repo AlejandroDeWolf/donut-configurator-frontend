b5e0fef0aaf52d976994f55f46659ec99709425a
AimaneMaadal <aimanemaadal@gmail.com>
2022-12-04 00:48:46 +0100

    feat: import three and create scene

---
+++ main.js
@@ -0,0 +1,110 @@
+import * as THREE from 'three';
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
+import {
+    GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+
+const configurator = document.getElementById('configurator');
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(configurator.clientWidth, configurator.clientHeight);
+renderer.setClearColor(0xFFFFFF, 1);
+
+configurator.appendChild(renderer.domElement);
+
+const FOV = 45;
+const ASPECT = configurator.clientWidth / configurator.clientHeight;
+const NEAR = 0.1;
+const FAR = 1000;
+const camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+camera.position.z = 5;
+
+const scene = new THREE.Scene();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 4;
+controls.maxDistance = 5;
+
+var Mesh = null;
+
+const loader = new GLTFLoader();
+loader.load('images/donut.glb', function (gltf) {
+    Mesh = gltf.scene;
+    Mesh.rotation.x = Math.PI / 4;
+    scene.add(Mesh);
+});
+
+
+console.log(Mesh);
+
+
+
+
+const pointLight = new THREE.PointLight(0xFFFFFF, 0.5);
+pointLight.position.set(10, 10, 10);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.6);
+scene.add(ambientLight);
+
+
+requestAnimationFrame(update);
+
+function update() {
+    requestAnimationFrame(update);
+    Mesh.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+
+
+
+const doughs = document.querySelectorAll('.dough');
+doughs.forEach(button => {
+    button.addEventListener('click', () => {
+        var dough = button.id;
+        for (var i = 0; i < Mesh.children.length; i++) {
+            if (Mesh.children[i].name.startsWith("Glaze")) {
+                Mesh.children[i].material.color.setHex(dough);
+            }
+        }
+    });
+});
+
+const glazes = document.querySelectorAll('.topping');
+glazes.forEach(button => {
+    button.addEventListener('click', () => {
+        for (var i = 0; i < Mesh.children.length; i++) {
+            var fullTopping = button.id;
+            var topping = fullTopping.substring(0, fullTopping.indexOf("_"));
+            var toppingName = fullTopping.substring(fullTopping.indexOf("_") + 1);
+            console.log(topping+" "+toppingName);
+            if (Mesh.children[i].name.startsWith(topping)) {
+                Mesh.children[i].visible = true;
+                if (toppingName) {
+                    Mesh.children[i].material.color.setHex(toppingName);
+                }
+            } 
+        }
+    });
+});
+
+
+
+
+
+
+window.addEventListener('load', function () {
+    for (var i = 0; i < Mesh.children.length; i++) {
+        //if name starts with Sprinkle then change color
+        if (Mesh.children[i].name.startsWith("Doughnut")) {
+            Mesh.children[i].material.color.setHex(0xf7e4c4);
+        } else if (Mesh.children[i].name.startsWith("Sprinkle")) {
+            Mesh.children[i].visible = false;
+        }
+    }
+});
